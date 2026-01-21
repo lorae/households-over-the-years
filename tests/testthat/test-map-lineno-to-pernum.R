2af5f2bdcc42dbee6921e3cd2e438ec6e18a352c
Lorae Stojanovic <stojanovic.lorae@gmail.com>
2026-01-20 20:18:03 -0500

    feat: confirm NA edge case

--- tests/testthat/test-map-lineno-to-pernum.R
+++ tests/testthat/test-map-lineno-to-pernum.R
@@ -85,3 +85,41 @@ test_that("convert_person_refs maps multiple columns from LINENO to PERNUM withi
   
   expect_equal(result, expected)
 })
+
+test_that("convert_person_refs handles NA values in reference columns", {
+  
+  input <- tibble(
+    hhid    = c(1, 1, 1, 2, 2, 2, 2),
+    perid   = c(1, 2, 3, 4, 5, 6, 7),
+    PERNUM  = c(1, 2, 3, 1, 2, 3, 4),
+    LINENO  = c(3, 1, 2, 2, 3, 4, 1),
+    MOMLOC  = c(0, 1, 1, 0 , 0, 2, 2),
+    PELNMOM = c(NA, 3, 3, 0, NA, 3, 3),
+    PELNDAD = c(0, NA, 0, NA, 0, 2, 2)
+  )
+  
+  expected <- tibble(
+    hhid    = c(1, 1, 1, 2, 2, 2, 2),
+    perid   = c(1, 2, 3, 4, 5, 6, 7),
+    PERNUM  = c(1, 2, 3, 1, 2, 3, 4),
+    LINENO  = c(3, 1, 2, 2, 3, 4, 1),
+    MOMLOC  = c(0, 1, 1, 0 , 0, 2, 2),
+    PELNMOM = c(NA, 3, 3, 0, NA, 3, 3),
+    PELNDAD = c(0, NA, 0, NA, 0, 2, 2),
+    PELNMOM_PERNUM = c(NA, 1, 1, 0, NA, 2, 2),
+    PELNDAD_PERNUM = c(0, NA, 0, NA, 0, 1, 1)
+  )
+  
+  result <- convert_person_refs(
+    df = input,
+    hhid_col = hhid,
+    target_ref_col = "PERNUM",
+    source_ref_col = "LINENO",
+    translate_cols = c("PELNMOM", "PELNDAD")
+  )
+  
+  print(result)
+  print(expected)
+  
+  expect_equal(result, expected)
+})
