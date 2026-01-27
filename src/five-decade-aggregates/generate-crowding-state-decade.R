7ef5d939f0aeffa97c2b7fb1a80f643fb51c2192
Lorae Stojanovic <stojanovic.lorae@gmail.com>
2026-01-27 16:40:15 -0500

    feat: import state FIPS codes

---
+++ src/five-decade-aggregates/generate-crowding-state-decade.R
@@ -0,0 +1,18 @@
+# ----- Step 0: ACS ----- #
+library("dplyr")
+library("duckdb")
+library("dbplyr")
+library("ggplot2")
+library("readr")
+library("tidyr")
+library("writexl")
+
+devtools::load_all("../demographr")
+
+# ----- Step 1: Connect to DB ----- #
+con <- dbConnect(duckdb::duckdb(), "data/db/ipums.duckdb")
+
+ipums_person <- tbl(con, "ipums_person") |>
+  mutate(crowded = ppbr > 2)
+
+base_data <- ipums_person |> filter(GQ %in% c(0, 1, 2))
