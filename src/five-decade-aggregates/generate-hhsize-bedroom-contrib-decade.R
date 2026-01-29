6c650b8b7263115d021e016c3da5679223d4ab7d
Lorae Stojanovic <stojanovic.lorae@gmail.com>
2026-01-29 14:32:45 -0500

    feat: calculate contributions from hhsize vs bedroom for intra-decade crowding changes

--- src/five-decade-aggregates/generate-hhsize-bedroom-contrib-decade.R
+++ src/five-decade-aggregates/generate-hhsize-bedroom-contrib-decade.R
@@ -10,4 +10,23 @@ out_dir  <- "output/five-decade-tables"
 hhsize   <- read_csv(file.path(data_dir, "hhsize_decade_overall.csv"), show_col_types = FALSE)
 bedrooms <- read_csv(file.path(data_dir, "bedroom_decade_overall.csv"), show_col_types = FALSE)
   
-  
+hhsize_bedrooms <- hhsize |>
+  left_join(
+    bedrooms |> select(YEAR, bedroom),
+    by = "YEAR"
+  ) |>
+  arrange(YEAR) |>
+  mutate(
+    decade_label = if_else(
+      is.na(lag(YEAR)),
+      NA_character_,
+      paste0(lag(YEAR), "-", YEAR)
+    ),
+    change_hhsize = hhsize - lag(hhsize),
+    change_bedroom = bedroom - lag(bedroom)
+  ) |>
+  relocate(decade_label, .after = YEAR)|>
+  mutate(
+    contrib_percent_hhsize = -change_hhsize / (change_bedroom - change_hhsize),
+    contrib_percent_bedroom = change_bedroom / (change_bedroom - change_hhsize)
+  )
